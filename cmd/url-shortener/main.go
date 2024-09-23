6c26c275f45906db0c770f73005907cc71f2a95e
StasTolmachov <stas@tolmachov.dev>
2024-09-23 07:56:38 +0200

    1

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,16 +2,47 @@ package main
 
 import (
 	"Shortener_Tuzov/internal/config"
+	"Shortener_Tuzov/internal/lib/logger/sl"
+	"Shortener_Tuzov/internal/storage/sqlite"
 	"fmt"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
 )
 
 func main() {
-	//init config: cleanenv
+	fmt.Println("start")
 	cfg := config.MustLoad()
 	fmt.Println(cfg)
-	//todo init logger: slog
+
+	log := setupLogger(cfg.Env)
+	log.Info("log info", slog.String("env", cfg.Env))
+
 	//todo init storage: sqlite
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+	_ = storage
 	//todo init router: chi
 	//todo run server
 
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	}
+	return log
 }
